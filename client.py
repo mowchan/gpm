0253106ff46bc1ab257fd778f1503edac8f5450f
Dan Mowchan <dan@mowchan.com>
2019-01-15 23:12:16 -0700

    rough artist search with artist/album selection

--- client.py
+++ client.py
@@ -7,11 +7,18 @@ from player import Player
 class Client:
     def __init__(self):
         # globals
+        self.artists = []
+        self.albums = []
+        self.tracks = {}
+        self.selected_artist = None
+        self.selected_album = None
         self.prompt_x = 2
         self.prompt_y = 0
         self.input_index = 0
         self.query = ''
         self.is_running = True
+        self.is_showing_artists = False
+        self.is_showing_albums = False
         # database, library, player
         self.init_database()
         self.init_library()
@@ -26,9 +33,10 @@ class Client:
 
     def init_library(self):
         self.music = Music(self.user_device_id)
-        if not self.db.is_library_ready():
-            songs = self.music.get_user_songs()
-            self.db.build_library(songs)
+        # if not self.db.is_library_ready():
+        # songs = self.music.get_user_songs()
+        # self.db.populate_tables(songs)
+        # TODO: populate_tables in a separate thread on launch if the db is already populated
 
     def init_player(self):
         self.player = Player()
@@ -89,7 +97,7 @@ class Client:
         # clear prompt window, draw text, refresh window
         self.scr_prompt.clear()
         self.scr_prompt.addstr(0, 0, '$', curses.color_pair(0))
-        self.scr_prompt.addstr(0, 3, 'artist - album', curses.color_pair(7))
+        self.scr_prompt.addstr(0, self.prompt_x, 'artist', curses.color_pair(7))
         self.scr_prompt.refresh()
         # set cursor position
         self.scr.move(self.prompt_y, self.prompt_x)
@@ -104,11 +112,11 @@ class Client:
         self.scr_np = self.scr.subwin(4, self.max_x - 2, 4, 2)
 
     def render_now_playing_text(self):
-        song = self.player.album[str(self.player.track_number)]
+        track = self.player.album['tracks'][str(self.player.track_number)]
         self.clear_now_playing()
-        self.scr_np.addstr(0, 0, song['title'], curses.color_pair(2) + curses.A_BOLD)
-        self.scr_np.addstr(1, 0, song['artist'], curses.color_pair(2) + curses.A_DIM)
-        self.scr_np.addstr(2, 0, song['album'], curses.color_pair(2) + curses.A_DIM)
+        self.scr_np.addstr(0, 0, track['name'], curses.color_pair(2) + curses.A_BOLD)
+        self.scr_np.addstr(1, 0, self.selected_artist['name'], curses.color_pair(2) + curses.A_DIM)
+        self.scr_np.addstr(2, 0, self.selected_album['name'], curses.color_pair(2) + curses.A_DIM)
         self.scr_np.refresh()
         # set cursor position
         self.scr.move(self.prompt_y + 1, self.prompt_x)
@@ -119,35 +127,100 @@ class Client:
         self.scr_np.refresh()
         self.scr.move(self.prompt_y + 1, self.prompt_x)
 
+    def render_artists(self, artists):
+        self.artists = artists
+        self.list_cur_y = 0
+        self.scr_list = self.scr.subwin(self.max_y - 8, self.max_x - 2, 4, 2)
+        for index, artist in enumerate(self.artists):
+            self.scr_list.addstr(index, 0, artist['name'], curses.color_pair(2))
+        self.scr.move(4, 2)
+        self.is_showing_artists = True
+        self.highlight_selected_list_item()
+
+    def get_active_list(self):
+        if self.is_showing_albums:
+            return self.albums
+        if self.is_showing_artists:
+            return self.artists
+
+    def highlight_selected_list_item(self):
+        active_list = self.get_active_list()
+        self.scr_list.chgat(self.list_cur_y, 0, len(active_list[self.list_cur_y]['name']), curses.A_REVERSE)
+        self.scr_list.refresh()
+
+    def increment_selected_list_item(self):
+        active_list = self.get_active_list()
+        cur_y, cur_x = self.scr.getyx()
+        self.scr.move(cur_y + 1, cur_x)
+        self.scr_list.chgat(self.list_cur_y, 0, len(active_list[self.list_cur_y]['name']), curses.color_pair(2))
+        self.list_cur_y += 1
+        self.highlight_selected_list_item()
+
+    def decrement_selected_list_item(self):
+        active_list = self.get_active_list()
+        cur_y, cur_x = self.scr.getyx()
+        self.scr.move(cur_y - 1, cur_x)
+        self.scr_list.chgat(self.list_cur_y, 0, len(active_list[self.list_cur_y]['name']), curses.color_pair(2))
+        self.list_cur_y -= 1
+        self.highlight_selected_list_item()
+
+    def select_list_item(self):
+        if self.is_showing_albums:
+            self.selected_album = self.albums[self.list_cur_y]
+            self.scr_list.clear()
+            self.is_showing_artists = False
+            self.is_showing_albums = False
+            self.play_album()
+            self.render_now_playing()
+        elif self.is_showing_artists:
+            self.selected_artist = self.artists[self.list_cur_y]
+            self.albums = self.selected_artist['albums']
+            self.scr_list.clear()
+            for index, album in enumerate(self.albums):
+                self.scr_list.addstr(index, 0, album['name'], curses.color_pair(2))
+            self.scr.move(4, 2)
+            self.list_cur_y = 0
+            self.scr_list.refresh()
+            self.is_showing_albums = True
+            self.highlight_selected_list_item()
+
     def handle_input(self, key):
         # self.player is loaded with URL; playing or paused
-        if not self.player is None and self.player.is_loaded: # player is loaded with url, playing or paused
+        if self.is_showing_albums or self.is_showing_artists:
             if key == curses.KEY_DOWN:
-                self.pause_song()
-            elif key == curses.KEY_RIGHT:
-                self.play_next_song()
-            elif key == curses.KEY_LEFT:
-                self.play_prev_song()
-            elif key == 27: # escape
-                self.clear_playback_timer()
-                self.clear_now_playing()
-                self.player.reset()
-        # no playback keys pressed; continue normal input handling
-        if key == 27:
-            self.is_running = False
-        # backspace, mac "delete" key
-        if key == curses.KEY_BACKSPACE or key == 127:
-            # the query is about to be cleared
-            if self.input_index < 2:
-                self.handle_input_clear()
-            else:
-                self.handle_input_backspace()
-        elif key == 10: # enter
-            self.handle_input_enter()
-        elif not key == curses.KEY_DOWN and not key == curses.KEY_UP and not key == curses.KEY_LEFT and not key == curses.KEY_RIGHT and not key == 27:
-            if self.input_index == 0:
-                self.render_input_start()
-            self.handle_input_concat(key)
+                self.increment_selected_list_item()
+            elif key == curses.KEY_UP:
+                self.decrement_selected_list_item()
+            elif key == 10: # enter
+                self.select_list_item()
+        else:
+            if not self.player is None and self.player.is_loaded: # player is loaded with url, playing or paused
+                if key == curses.KEY_DOWN:
+                    self.pause_song()
+                elif key == curses.KEY_RIGHT:
+                    self.play_next_song()
+                elif key == curses.KEY_LEFT:
+                    self.play_prev_song()
+                elif key == 27: # escape
+                    self.clear_playback_timer()
+                    self.clear_now_playing()
+                    self.player.reset()
+            # no playback keys pressed; continue normal input handling
+            if key == 27:
+                self.is_running = False
+            # backspace, mac "delete" key
+            if key == curses.KEY_BACKSPACE or key == 127:
+                # the query is about to be cleared
+                if self.input_index < 2:
+                    self.handle_input_clear()
+                else:
+                    self.handle_input_backspace()
+            elif key == 10: # enter
+                self.handle_input_enter()
+            elif not key == curses.KEY_DOWN and not key == curses.KEY_UP and not key == curses.KEY_LEFT and not key == curses.KEY_RIGHT and not key == 27:
+                if self.input_index == 0:
+                    self.render_input_start()
+                self.handle_input_concat(key)
 
     def handle_input_clear(self):
         self.query = ''
@@ -164,10 +237,11 @@ class Client:
         self.scr.move(self.prompt_y, self.prompt_x + self.input_index)
 
     def handle_input_enter(self):
-        album = self.search_query()
-        if album:
+        results = self.db.search_artists(self.query)
+        if len(results) > 0:
+            self.clear_now_playing()
             self.handle_input_clear()
-            self.play_album(album)
+            self.render_artists(results)
 
     def handle_input_concat(self, key):
         character = chr(key)
@@ -176,26 +250,19 @@ class Client:
         self.scr_prompt.refresh()
         self.input_index += 1
 
-    def search_query(self):
-        if len(self.query) == 0:
-            return False
-        else:
-            the_query = [x.strip() for x in self.query.split('-')]
-            return self.db.get_album(the_query[0], the_query[1])
-
-    def play_album(self, album):
+    def play_album(self):
         self.player.reset()
-        self.player.set_album(album)
+        self.player.set_album(self.selected_album)
         self.play_song()
 
     def play_song(self):
         try:
-            song = self.player.album[str(self.player.track_number)]
-            song_duration = int(song['durationMillis']) / 1000
-            song_url = self.music.get_song_stream_url(song['id'])
-            self.player.load_url(song_url)
+            track = self.player.album['tracks'][str(self.player.track_number)]
+            track_duration = track['duration']
+            track_url = self.music.get_song_stream_url(track['stream_id'])
+            self.player.load_url(track_url)
             self.player.play()
-            self.player.playback_timer = Timer(song_duration, self.play_next_song)
+            self.player.playback_timer = Timer(track_duration, self.play_next_song)
             self.player.playback_timer.start()
             self.render_now_playing_text()
         except:
--- database.py
+++ database.py
@@ -1,61 +1,119 @@
 import os
-from tinydb import TinyDB, Query
+import sqlite3 as lite
 
 class Database:
     def __init__(self):
-        self.json_dir = os.path.expanduser('~') + '/.gpm/'
-        self.json_name = 'gpm.json'
-        self.create_json()
-        self.db = TinyDB(self.json_dir + self.json_name, default_table='gpm')
-        self.user = Query()
-
-    def create_json(self):
-        if not os.path.exists(self.json_dir):
-            os.makedirs(self.json_dir)
-            open(self.json_dir + self.json_name, 'w+')
-
-    def set_user_device_id(self, user_device_id):
-        if self.db.contains(self.user.device_id):
-            # TODO: overwrite device_id
-            print('todo... already exists')
-        else:
-            self.db.insert({'device_id': user_device_id})
-
+        db_path = os.path.expanduser('~') + '/.gpm/'
+        db_file = 'gpm.db'
+        self.con = lite.connect(db_path + db_file)
+        self.db = self.con.cursor()
+        self.init_tables()
+    
+    def init_tables(self):
+        # create tables
+        self.db.execute('CREATE TABLE IF NOT EXISTS user(device_id TEXT)')
+        self.db.execute('CREATE TABLE IF NOT EXISTS artists(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)')
+        self.db.execute('CREATE TABLE IF NOT EXISTS albums(id INTEGER PRIMARY KEY AUTOINCREMENT, artist_id INT, name TEXT)')
+        # TODO: support multiple artists on an album
+        self.db.execute('CREATE TABLE IF NOT EXISTS tracks(id INTEGER PRIMARY KEY AUTOINCREMENT, album_id INT, name TEXT, stream_id TEXT, disc_number INT, track_number INT, duration INT)')
+        # self.db.execute('INSERT INTO user VALUES("DEVICE_ID")')
+        # self.con.commit()
+ 
     def get_user_device_id(self):
-        if self.db.contains(self.user.device_id):
-            return self.db.get(self.user.device_id)['device_id']
-        else:
-            # TODO: handle no device_id with exception?
-            print('no device_id')
+        self.db.execute('SELECT device_id FROM user')
+        return self.db.fetchone()[0]
 
-    def is_library_ready(self):
-        return self.db.contains(self.user.library)
-
-    def build_library(self, songs):
-        library = {}
-
+    def populate_tables(self, songs):
         for song in songs:
-            artist = song['artist']
-            album = song['album']
-            trackNumber = song['trackNumber']
+            artist_name = song['artist']
+            album_name = song['album']
+            disc_number = song['discNumber']
+            duration = int(song['durationMillis']) / 1000
+            track_name = song['title']
+            track_number = song['trackNumber']
+            track_stream_id = song['id']
+            artist_id = self.insert_artist(artist_name)
+            album_id = self.insert_album(album_name, artist_id)
+            track_id = self.insert_track(track_name, track_stream_id, disc_number, track_number, album_id, duration)
+        self.con.commit()
 
-            if artist in library:
-                # artist exists in db...
-                if album in library[artist]:
-                    # album exists for artist in db, add song
-                    library[artist][album][trackNumber] = song
-                else:
-                    # create album for artist, add song
-                    library[artist][album] = {}
-                    library[artist][album][trackNumber] = song
-            else:
-                # create artist, create album for artist, add song
-                library[artist] = {}
-                library[artist][album] = {}
-                library[artist][album][trackNumber] = song
+    def insert_artist(self, artist_name):
+        self.db.execute('SELECT id FROM artists WHERE name=?', (artist_name,))
+        artist = self.db.fetchone()
 
-        self.db.insert({ 'library': library })
+        if artist is None:
+            # artist does not exist yet, insert it
+            self.db.execute('INSERT INTO artists VALUES(NULL, ?)', (artist_name,))
+            # return id of the inserted row
+            return self.db.lastrowid
+        else:
+            # artist exists, return id
+            return artist[0]
 
-    def get_album(self, artist, album):
-        library = self.db.get(self.user.library)['library']
-        return library[artist][album]
+    def insert_album(self, album_name, artist_id):
+        self.db.execute('SELECT id FROM albums WHERE name=? AND artist_id=?', (album_name, artist_id))
+        album = self.db.fetchone()
+
+        if album is None:
+            # album does not exist yet, insert it
+            self.db.execute('INSERT INTO albums VALUES(NULL, ?, ?)', (artist_id, album_name))
+            # return id of the inserted row
+            return self.db.lastrowid
+        else:
+            # album exists, return id
+            return album[0]
+
+    def insert_track(self, track_name, track_stream_id, disc_number, track_number, album_id, duration):
+        self.db.execute('SELECT id FROM tracks WHERE name=? AND disc_number=? AND track_number=? AND album_id=?', (track_name, disc_number, track_name, album_id))
+        track = self.db.fetchone()
+
+        if track is None:
+            # track does not exist yet, insert it
+            self.db.execute('INSERT INTO tracks VALUES(NULL, ?, ?, ?, ?, ?, ?)', (album_id, track_name, track_stream_id, disc_number, track_number, duration))
+            # return id of the inserted row
+            return self.db.lastrowid
+        else:
+            # track exists, return id
+            return track[0]
+
+    def search_artists(self, query_string):
+        self.db.execute('SELECT * FROM artists WHERE name LIKE ?', ('%' + query_string + '%',))
+        artists = self.db.fetchall()
+        artists_normalized = []
+
+        for artist in artists:
+            artist_normalized = {
+                'id': artist[0],
+                'name': artist[1],
+                'albums': []
+            }
+            self.db.execute('SELECT * FROM albums WHERE artist_id=?', (artist_normalized['id'],))
+            albums = self.db.fetchall()
+
+            for album in albums:
+                album_normalized = {
+                    'id': album[0],
+                    'name': album[2]
+                }
+                self.db.execute('SELECT * FROM tracks WHERE album_id=?', (album[0],))
+                tracks = self.db.fetchall()
+                tracks_normalized = {}
+
+                for track in tracks:
+                    track_number = track[5]
+                    tracks_normalized[str(track_number)] = {
+                        'name': track[2],
+                        'stream_id': track[3],
+                        'disc_number': track[4],
+                        'duration': track[6],
+                        'track_number': track_number
+                    }
+
+                album_normalized['tracks'] = tracks_normalized
+                artist_normalized['albums'].append(album_normalized)
+
+            artists_normalized.append(artist_normalized)
+
+        return artists_normalized
+
+Database()
